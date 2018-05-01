29cb332c67bae758584b3ad1707db2ff3e6be1b1
developer119 <developer119korea@gmail.com>
2018-05-01 23:37:23 +0900

    투두리스트 기능 구현

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -40,4 +40,4 @@ const mapDispatchToProps = (dispatch) => ({
 // 컴포넌트를 리덕스와 연동 할 떄에는 connect 를 사용합니다.
 // connect() 의 결과는, 컴포넌트에 props 를 넣어주는 함수를 반환합니다.
 // 반환된 함수에 우리가 만든 컴포넌트를 넣어주면 됩니다.
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);ㄹ
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
