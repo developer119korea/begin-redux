93fdf9d0f11807298eaa6cd24869fb9c1c9d9aba
YoungwanCho <developer119korea@gmail.com>
2021-01-12 23:25:47 +0900

    투두리스트에 리덕스를 사용

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,1 +1,18 @@
-// 카운터 관련 상태 로직
+import { createAction, handleActions } from 'redux-actions';
+
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+export const increment = createAction(INCREMENT);
+export const decrement = createAction(DECREMENT);
+
+const initialState = {
+  number: 0
+};
+
+export default handleActions({
+  [INCREMENT] : (state, action) => {
+    return { number: state.number + 1 };
+  },
+  [DECREMENT] : ({number}) => ({number: number - 1})
+}, initialState);
